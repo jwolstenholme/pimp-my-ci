9ca466467fd8a99baed06aa3d04aa2efa34c40f4
James Wolstenholme <j@meswolstenholme.com>
2014-04-08 09:31:41 +1000

    Jenkins monitor is only sending updates for status differences

--- monitors/jenkins_monitor.py
+++ monitors/jenkins_monitor.py
@@ -1,26 +1,34 @@
 from lib.const import *
 
+status_dict = {
+  'aborted_anime'   : BUILDING_FROM_UNKNOWN,
+  'blue'            : SUCCESS,
+  'blue_anime'      : BUILDING_FROM_SUCCESS,
+  'disabled_anime'  : BUILDING_FROM_UNKNOWN,
+  'grey_anime'      : BUILDING_FROM_UNKNOWN,
+  'notbuilt_anime'  : BUILDING_FROM_UNKNOWN,
+  'red'             : FAILURE,
+  'red_anime'       : BUILDING_FROM_FAILURE,
+  'yellow_anime'    : BUILDING_FROM_UNKNOWN
+}
+
 class JenkinsMonitor:
 
   def __init__(self, job_queues):
     self.job_queues = job_queues
-
-    self.status_dict = {
-      'aborted_anime'   : BUILDING_FROM_UNKNOWN,
-      'blue'            : SUCCESS,
-      'blue_anime'      : BUILDING_FROM_SUCCESS,
-      'disabled_anime'  : BUILDING_FROM_UNKNOWN,
-      'grey_anime'      : BUILDING_FROM_UNKNOWN,
-      'notbuilt_anime'  : BUILDING_FROM_UNKNOWN,
-      'red'             : FAILURE,
-      'red_anime'       : BUILDING_FROM_FAILURE,
-      'yellow_anime'    : BUILDING_FROM_UNKNOWN
-    }
+    self.job_statuses = dict.fromkeys(job_queues.keys())
 
   def process_build(self, build):
     job_statuses = self.__parse_build(build)
-    for build, status in job_statuses.iteritems():
+    differences = self.__filter_differences(self.job_statuses, job_statuses)
+
+    for build, status in differences.iteritems():
       self.job_queues[build].put_nowait(status)
+
+    self.job_statuses = job_statuses
+
+  def __filter_differences(self, old_builds, new_builds):
+    return dict(new_builds.viewitems() - old_builds.viewitems())
 
   # return true for only the jobs we're interested in
   def __filter_build(self, build):
@@ -28,7 +36,7 @@ class JenkinsMonitor:
 
   # map jenkins job entries to our jobs
   def __jenkins_to_rpi(self, job):
-    return { job['name'] : self.status_dict.get(job['color'], UNKNOWN) }
+    return { job['name'] : status_dict.get(job['color'], UNKNOWN) }
 
   # returns dictionary of build_name to current status
   def __parse_build(self, build):
