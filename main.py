6fdad0c1faa5fefbec00e12999f9f1841ee35e85
James Wolstenholme <j@meswolstenholme.com>
2014-07-09 15:31:27 +1000

    Adds android monkey talk job

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def main():
     build_jobs = [
         BuildJob(name='Monitor U 01 Channel Arrangement', num_leds=num_leds),
         BuildJob(name='Truman-ios',                       num_leds=num_leds),
-        BuildJob(name='MonkeyTalk',                       num_leds=num_leds),
+        BuildJob(name='MonkeyTalk',                       num_leds=2, offset=0),
+	BuildJob(name='Android_Monkey',                   num_leds=2),
         BuildJob(name='Android_Commit',                   num_leds=2, offset=0),
         BuildJob(name='Android_Functional',               num_leds=2, offset=0),
         BuildJob(name='Android_Hockey_Deploy',            num_leds=1),
