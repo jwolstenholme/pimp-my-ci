6e448d3b9b49e01156d1ccf0cf0607b7db33be91
James Wolstenholme <j@meswolstenholme.com>
2014-07-24 09:48:00 +1000

    Added DAM to build lights

--- main.py
+++ main.py
@@ -27,16 +27,17 @@ log = logging.getLogger()
 
 def main():
     # TODO config
-    num_leds = 5
+    num_leds = 4
     build_jobs = [
-        BuildJob(name='Monitor U 01 Channel Arrangement', num_leds=num_leds),
-        BuildJob(name='Truman-ios',                       num_leds=num_leds),
-        BuildJob(name='MonkeyTalk',                       num_leds=2, offset=0),
-	BuildJob(name='Android_Monkey',                   num_leds=2),
-        BuildJob(name='Android_Commit',                   num_leds=2, offset=0),
-        BuildJob(name='Android_Functional',               num_leds=2, offset=0),
-        BuildJob(name='Android_Hockey_Deploy',            num_leds=1),
-        BuildJob(name='ChannelApi',                       num_leds=num_leds)
+        BuildJob(name='Monitor U 01 Channel Arrangement',   num_leds=num_leds),
+        BuildJob(name='Truman-ios',                         num_leds=num_leds),
+        BuildJob(name='MonkeyTalk',                         num_leds=2, offset=0),
+        BuildJob(name='Android_Monkey',                     num_leds=2),
+        BuildJob(name='Android_Commit',                     num_leds=2, offset=0),
+        BuildJob(name='Android_Functional',                 num_leds=2, offset=0),
+        BuildJob(name='Android_Hockey_Deploy',              num_leds=1),
+        BuildJob(name='ChannelApi',                         num_leds=num_leds),
+        BuildJob(name='DAM',                                num_leds=num_leds)
     ]
 
     job_queues = {job.name: Queue.Queue() for job in build_jobs}
