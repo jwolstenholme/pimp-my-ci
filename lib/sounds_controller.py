ac03e412e573299654cb6f658f315042adfecbc9
James Wolstenholme <j@meswolstenholme.com>
2014-08-25 17:51:22 +1000

    [Sounds] All quiet for the first run, play sounds on subsequent builds

--- lib/sounds_controller.py
+++ lib/sounds_controller.py
@@ -6,10 +6,13 @@ class SoundsController:
 
   def __init__(self, job_queues):
     self.sound_player = Player()
+    self.play_sounds = dict.fromkeys(job_queues.keys(), False)
 
-  def update_build_status(self, build, status):
-    if (status == SUCCESS):
-      self.sound_player.play_random_success_sound()
-    elif (status == FAILURE):
-      self.sound_player.play_random_failure_sound()
-
+  def update_build_status(self, job, status):
+    if (self.play_sounds[job]):
+      if (status == SUCCESS):
+        self.sound_player.play_random_success_sound()
+      elif (status == FAILURE):
+        self.sound_player.play_random_failure_sound()
+    else:
+      self.play_sounds[job] = True
