99e671f936c63eb344d6815b23c2f71226f6c1c7
James Wolstenholme <j@meswolstenholme.com>
2014-08-25 15:34:39 +1000

    [Config] Added request/response check for configuration url + output

--- check_config.py
+++ check_config.py
@@ -1,27 +1,38 @@
 
 from config.config import Config
 
+import urllib2
+import yaml
+
 class Check:
 
   def __init__(self):
-    config = Config()
 
     print "\n==== Global ===="
-    print "  CI server url    : ", config.ci_url
-    print "  Polling interval : ", config.polling_interval_secs, "seconds"
+    print "  CI server url    : ", Config.ci_url
+    print "  Polling interval : ", Config.polling_interval_secs, "seconds"
 
     print "\n==== Builds ===="
     index = 0
     job_leds = dict()
     for job in Config.jobs:
-      print job.name.ljust(16), "leds", job.led_coordinates(index)
+      print " ", job.name.ljust(16), "leds", job.led_coordinates(index)
       for i in job.led_addresses(index):
         job_leds[i] = job.name
       index = job.next_index(index)
 
     print "\n==== LEDs ===="
     for i in range(Config.total_number_leds):
-      print " [", str(i).rjust(2), "]", job_leds.get(i, "  *")
+      print "  [", str(i).rjust(2), "]", job_leds.get(i, "  *")
+
+    print "\n==== Test ===="
+    try:
+      req = urllib2.Request(Config.ci_url)
+      req.add_header('Content-Type', 'application/json')
+      response_body = urllib2.urlopen(req).read()
+      print yaml.load(response_body)
+    except:
+      print "Error:", sys.exc_info()[0]
 
 if __name__ == '__main__':
     Check()
