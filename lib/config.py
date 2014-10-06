1d11a02a5b6e1ef01d50939cfbae3082f4df67de
James Wolstenholme <j@meswolstenholme.com>
2014-10-06 14:43:16 +1100

    [Config] Updated demo config IP address

--- lib/config.py
+++ lib/config.py
@@ -5,7 +5,7 @@ class Config:
   total_number_leds = 32
 
   # The URL of the CI server
-  ci_url = 'http://192.168.0.69:8080/jenkins/api/json'
+  ci_url = 'http://192.168.0.40:8080/jenkins/api/json'
 
   # The number of seconds to wait between polling the CI server
   polling_interval_secs = 3
